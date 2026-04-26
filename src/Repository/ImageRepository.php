84e4603009b7ccf30dcd2212a5cba810badb6df8
quiestgris <sbaev72300@gmail.com>
2026-04-26 22:25:13 +0200

    v1.1

--- src/Repository/ImageRepository.php
+++ src/Repository/ImageRepository.php
@@ -16,7 +16,7 @@ class ImageRepository extends ServiceEntityRepository
         parent::__construct($registry, Image::class);
     }
 
-    public function findBySrcContains(string $substring): ?array
+    public function findBySrcContains(string $substring): ?Image
     {
         return $this->createQueryBuilder('i')
             ->where('i.src LIKE :substr')
